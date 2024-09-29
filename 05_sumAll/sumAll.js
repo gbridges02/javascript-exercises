57705f4d8cff0700ae14d5434cb3d8b73abea507
gbridges02 <gsbdd02@gmail.com>
2024-09-29 19:29:40 -0400

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    let smallerNum, largerNum;
 
+    if(firstNum < 0 || lastNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(lastNum)){
+        return "ERROR";
+    }
+
+    if(firstNum <= lastNum){
+        smallerNum = firstNum;
+        largerNum = lastNum;
+    }
+    else{
+        smallerNum = lastNum;
+        largerNum = firstNum;
+    }
+
+    for(let i = smallerNum; i <= largerNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
